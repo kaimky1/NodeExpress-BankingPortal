3de3a5fbdea0f4802cc2273d986a8d54cece791d
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-06-12 08:57:50 -1000

    Building out back end

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require('express');
+const app = express();
+
+
+app.set()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
